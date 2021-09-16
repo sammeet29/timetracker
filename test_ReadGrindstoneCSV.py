785f986b802a79feb7016e572773e63904184aae
Sammeet Koli <sammeet29@gmail.com>
2021-09-15 22:12:27 -0700

    Add python unittest file

---
+++ test_ReadGrindstoneCSV.py
@@ -0,0 +1,19 @@
+import unittest
+import datetime
+from read_grindstone_csv import Time_slice
+
+class Time_slice_test(unittest.TestCase):
+    def test_expected_working(self):
+        WORK_ITEM = '1181-ROS QSPI FT'
+        START_TIME = '8/16/2021 10:39:16 AM'
+        DURATION =  '0:15:32'
+
+
+        tc = Time_slice(WORK_ITEM, START_TIME, DURATION)
+
+        self.assertTrue(tc.work_item == WORK_ITEM, 'Work Item does not match')
+        self.assertTrue(tc.start == START_TIME, 'Start time is not correct')
+        self.assertTrue(tc.duration == DURATION)
+
+if __name__ == "__main__":
+    unittest.main()
