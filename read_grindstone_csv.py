f9f31029c40f5c3f971d8d4303fe01580df32e32
Sammeet Koli <sammeet29@gmail.com>
2021-09-27 22:27:11 -0700

    Make timeslice store and return only the start time date

--- read_grindstone_csv.py
+++ read_grindstone_csv.py
@@ -4,7 +4,8 @@ from datetime import datetime
 class Time_slice:
     def __decode_date(self, start_date_time):
         format = "%m/%d/%Y %I:%M:%S %p"
-        return datetime.strptime(start_date_time, format)
+        tc_start = datetime.strptime(start_date_time, format)
+        return tc_start.date()
 
     def __init__(self, work_item, start, duration):
         self.work_item = work_item
@@ -14,6 +15,9 @@ class Time_slice:
 
     def __str__(self):
         return "Name: %s Start:%s Duration:%s" %(self.work_item, self.start, self.duration)
+
+    def get_date(self):
+        return self.day_date
 
 class Day_items:
     def __init__(self, day_date):
--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
 Create a time tracker tool that interacts with JIRA
+
+### How to run Utests:
+Inside the tests folder, run the following command
+'''
+python -m unittest -v
+'''
