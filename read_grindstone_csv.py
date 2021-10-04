0e4c975fdbffa245b86559ac9224880e1fee1044
Sammeet Koli <sammeet29@gmail.com>
2021-10-03 20:02:51 -0700

    Add checks for correct columns

--- read_grindstone_csv.py
+++ read_grindstone_csv.py
@@ -1,8 +1,22 @@
 import csv
+from grindstone_timeslice import Time_slice
+from os.path import exists
 
 class Read_grindstone_csv:
-    def is_valid(self):
-        return False
+    def __init__(self, file_name):
+        if(exists(file_name)):
+            with open(file_name, encoding='utf-8-sig') as csv_file:
+                first_line = csv_file.readline()
+                self.is_valid = self.__has_valid_headers(first_line)
+        else:
+            self.is_valid = False
+
+    def __has_valid_headers(self, first_line):
+        if(first_line.find("Start") == -1 or
+        first_line.find("Duration") == -1 or
+        first_line.find("Work Item") == -1):
+            return False
+        return True
 
 class Day_items:
     def __init__(self, day_date):
--- tests/test_ReadGrindstoneCSV.py
+++ tests/test_ReadGrindstoneCSV.py
@@ -6,10 +6,14 @@ sys.path.append('../')
 from read_grindstone_csv import Read_grindstone_csv
 
 class read_grindstone_csv(unittest.TestCase):
-    def test_expected_working(self):
-        read_file = Read_grindstone_csv()
-        self.assertTrue(read_file.is_valid())
+    def test_invalid_filepath(self):
+        read_file = Read_grindstone_csv("events.csv")
+        self.assertFalse(read_file.is_valid)
 
+    def test_invalied_csv(self):
+        read_file = Read_grindstone_csv("invalid_timeslices.csv")
+        self.assertFalse(read_file.is_valid)
 
-if __name__ == "__main__":
-    unittest.main()
+    def test_valid_filepath(self):
+        read_file = Read_grindstone_csv("test.csv")
+        self.assertTrue(read_file.is_valid)
