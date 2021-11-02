acb074f0d6c31b8c72656fd319e2fc6dcf53761c
Sammeet Koli <sammeet_koli@selinc.com>
2021-11-02 10:21:14 -0700

    Add worklog function

--- jira_helper.py
+++ jira_helper.py
@@ -6,6 +6,11 @@ import getpass
 import json
 import requests
 
+'''
+JIRA rest v2 reference docs
+https://developer.atlassian.com/cloud/jira/platform/rest/v2/intro/
+'''
+
 class Jira_helper:
     def __init__(self):
         self.__authenticate__()
@@ -23,13 +28,32 @@ class Jira_helper:
         print(issue.url)
         return issue
 
+
+    '''
+    https://docs.atlassian.com/software/jira/docs/api/REST/1000.824.0/#api/2/issue-addWorklog
+    https://developer.atlassian.com/cloud/jira/platform/rest/v2/api-group-issue-worklogs/#api-rest-api-2-issue-issueidorkey-worklog-post
+    '''
+    def add_work_log(self, issue_id, time_in_secs):
+        url = SEL_JIRA_URL + "issue/" + issue_id + "/worklog"
+        payload = json.dumps({
+            "timeSpentSeconds" : time_in_secs
+        })
+        headers = {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        }
+        return requests.post(url, auth=(self.user, self.pswd), data=payload, headers=headers)
+
     def __authenticate__(self):
         self.user = getpass.getuser()
         self.pswd = getpass.getpass(prompt= ("Password for " + self.user + ": "))
 
 def main():
     j = Jira_helper()
-    issue = j.find_issue('RP-8054')
+    TEST_ISSUE = 'RP-8054'
+    # issue = j.find_issue('RP-8054')
+    # issue = j.get_open_issues()
+    issue = j.add_work_log(TEST_ISSUE, 900)
     print("Issue Response :", issue)
     if(issue is not None):
         issue_json = issue.json()
