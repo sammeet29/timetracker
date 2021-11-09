667b427f99bf44d05f3611f353a7c1e6496b61a1
Sammeet Koli <sammeet29@gmail.com>
2021-11-08 22:10:18 -0800

    Add mock checks for get and post calls

--- tests/test_jira_helper.py
+++ tests/test_jira_helper.py
@@ -20,3 +20,15 @@ class Jira_helper_test(unittest.TestCase):
 
         self.assertEqual(self.j_helper.user, 'sammkoli')
         self.assertEqual(self.j_helper.pswd, 'passwd')
+
+    @mock.patch('requests.get')
+    def test_find_issue(self, mock_request_get):
+        TEST_ISSUE = 'RP-8054'
+        r = self.j_helper.find_issue(TEST_ISSUE)
+        mock_request_get.assert_called()
+
+    @mock.patch('requests.post')
+    def test_add_work_log(self, mock_request_post):
+        TEST_ISSUE = 'RP-8054'
+        r = self.j_helper.add_work_log(TEST_ISSUE, 900)
+        mock_request_post.assert_called()
